774e585f688ad370d85dc61551e11c2f42533f5c
Mustapha.Elkamal <mustapha.elkamal@gmail.com>
2021-09-09 11:52:22 +0100

    Full Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('And here we forked it'));
+app.get('/', (req, res) => res.send('And here we pipelined It'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
